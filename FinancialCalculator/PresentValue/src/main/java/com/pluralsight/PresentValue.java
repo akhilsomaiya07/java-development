003aa6438968cc0e739be4399c60bdfee81cbf42
akhilsomaiya07 <akhilsomaiya7@gmail.com>
2023-10-13 23:06:26 -0400

    FullNameApplication

--- FinancialCalculator/PresentValue/src/main/java/com/pluralsight/PresentValue.java
+++ FinancialCalculator/PresentValue/src/main/java/com/pluralsight/PresentValue.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
-public class PresentValue { 
+public class PresentValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
